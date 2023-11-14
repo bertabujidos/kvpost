0c3a68394de20cca53f9dae71ad9015bfb4505cc
Berta Bujidos Santomé <53273959+bertabujidos@users.noreply.github.com>
2023-11-14 20:27:27 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('kv-form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const key = formData.get('key');
+    const value = formData.get('value');
+
+    try {
+        const response = await fetch('https://workers-kv-demo.berta.workers.dev', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ key, value }),
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to store data');
+        }
+
+        const result = await response.text();
+        document.getElementById('result').innerText = `Data stored successfully: ${result}`;
+    } catch (error) {
+        document.getElementById('result').innerText = `Error: ${error.message}`;
+    }
+});
